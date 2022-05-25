fb6c0abd16e5663af0676af9cc695415939c98e8
Elijah Whalen <eliwhalen3@gmail.com>
2022-05-24 21:24:40 -0600

    Built out road, car, and sensors

---
+++ main.js
@@ -0,0 +1,26 @@
+// Set up our canvas dimensions
+const canvas = document.getElementById("carCanvas");
+
+canvas.width = 200;
+
+// Get 2d context for the canvas
+const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+
+// Animate everything
+animate();
+function animate() {
+  car.update();
+
+  canvas.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+
+  road.draw(ctx);
+  car.draw(ctx);
+
+  ctx.restore();
+  requestAnimationFrame(animate);
+}
